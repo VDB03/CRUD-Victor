284392c5434d1a4e7d49502ceda121c363523213
Victor Benitez <victordanielbenitez2003@gmail.com>
2024-12-01 17:09:03 -0300

    conexion a bbdd

--- app/models/mainModel.php
+++ app/models/mainModel.php
@@ -1,5 +1,6 @@
 <?php
     namespace app\models;
+    use \PDO;
 
     if(file_exists(__DIR__."/../../config/server.php")){
         require_once __DIR__."/../../config/server.php";
@@ -11,5 +12,12 @@
         private $db=DB_NAME;
         private $user=DB_USER;
         private $pass=DB_PASS;
+
+
+        protected function conectar (){
+            $conexion = new PDO("mysql:host=".$this->server.";dbname=".$this->db, $this->user, $this->pass);
+            $conexion->exec("SET CHARACTER SET utf8");
+            return $conexion;
+        }
 
     }
