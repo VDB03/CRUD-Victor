fec2767d05d93af27a7bade60c1be158957adbf4
Victor Benitez <victordanielbenitez2003@gmail.com>
2024-12-01 04:16:20 -0300

    cambios en config

---
+++ config/app.php
@@ -0,0 +1,6 @@
+<?php
+
+    const APP_URL="http://localhost/CRUD%20victor/";
+    const APP_NAME="CRUD POO MySQL";
+    const APP_SESSION_NAME="CRUD";
+    date_default_timezone_set("America/Argentina/Buenos_Aires");
---
+++ config/server.php
@@ -0,0 +1,6 @@
+<?php
+
+    const DB_SERVER="localhost";
+    const DB_NAME="crud victor";
+    const DB_USER="ROOT";
+    const DB_PASS="";
