c8b6229bd116c46501fce1b57b6f3c23e46d6aad
Victor Benitez <victordanielbenitez2003@gmail.com>
2024-12-01 17:40:42 -0300

    filtros inyeccion

--- app/models/mainModel.php
+++ app/models/mainModel.php
@@ -26,4 +26,20 @@
         return $sql;
         }
 
+        public function limpiarCadena($cadena){
+            $palabras=["<script>","</script>","<script src","<script type=","SELECT * FROM","SELECT "," SELECT ","DELETE FROM","INSERT INTO","DROP TABLE","DROP DATABASE","TRUNCATE TABLE","SHOW TABLES","SHOW DATABASES","<?php","?>","--","^","<",">","==","=",";","::"];
+            $cadena=trim($cadena);
+            $cadena=stripslashes($cadena);
+
+            foreach($palabras as $palabra){
+                $cadena=str_ireplace($palabra, "", $cadena);
+
+            }
+
+            $cadena=trim($cadena);
+            $cadena=stripslashes($cadena);
+
+            return $cadena;
+        }
+
     }
