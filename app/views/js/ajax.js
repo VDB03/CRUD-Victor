b3158201b8c200823437f4f296141b1ed67dc121
Victor Benitez <victordanielbenitez2003@gmail.com>
2024-12-02 13:38:15 -0300

    alertas ajax

--- app/views/js/ajax.js
+++ app/views/js/ajax.js
@@ -43,4 +43,46 @@ formularios_ajax.forEach(formularios => {
 
 function alertas_ajax(alerta){
 
+    if(alerta.tipo=="simple"){
+      
+      Swal.fire({
+        icon: alerta.icono,
+        title: alerta.titulo,
+        text: alerta.texto,
+        confirmButtonText: 'aceptar'
+      });
+
+    }else if(alerta.tipo=="recargar"){
+
+      Swal.fire({
+        icon: alerta.icono,
+        title: alerta.titulo,
+        text: alerta.texto,
+        confirmButtonText: 'aceptar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+            location.reload();
+        }
+      });
+
+    }else if(alerta.tipo=="limpiar"){
+
+      Swal.fire({
+        icon: alerta.icono,
+        title: alerta.titulo,
+        text: alerta.texto,
+        confirmButtonText: 'aceptar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+            document.querySelector(".FormularioAjax")
+            .reset()
+        }
+      });
+      
+    }else if(alerta.tipo=="redireccionar"){
+
+      window.location.href=alerta.url;
+
+    }
+
 }
