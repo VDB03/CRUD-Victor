0d1daf29f2295a9f5aa38ef135fa6ca178eb5910
Victor Benitez <victordanielbenitez2003@gmail.com>
2024-12-10 00:06:22 -0300

    final

--- app/controllers/userController.php
+++ app/controllers/userController.php
@@ -261,4 +261,139 @@
                 }
                 return json_encode($alerta);
         }
+
+        #listar usuarios#
+        public function listarUsuarioControlador($pagina,$registros,$url,$busqueda){
+
+                $nombre=$this->limpiarCadena($pagina);
+                $nombre=$this->limpiarCadena($registros);
+
+                $url=$this->limpiarCadena($url);
+                $url=APP_URL."/";
+
+                $busqueda=$this->limpiarCadena($busqueda);
+                $tabla="";
+
+                $pagina= (isset($pagina)  &&  $pagina>0) ? (int) $pagina : 1;
+                $inicio = ($pagina>0) ? (($pagina*$registros)-$registros) : 0 ;
+
+                if(isset($busqueda) && $busqueda!=""){
+
+                    $consulta_datos="SELECT * FROM usuario WHERE ((usuario_id!='".$_SESSION['id']."' AND usuario_id!='1') AND 
+                    (usuario_nombre LIKE '%$busqueda%' 
+                     OR usuario_apellido LIKE '%$busqueda%' 
+                     OR usuario_email LIKE '%$busqueda%' 
+                     OR usuario_usuario LIKE '%$busqueda%')) 
+                     ORDER BY usuario_nombre ASC LIMIT $inicio,$registros";
+
+                    $consulta_total="SELECT COUNT(usuario_id) FROM usuario WHERE ((usuario_id!='".$_SESSION['id']."' AND usuario_id!='1') AND 
+                    (usuario_nombre LIKE '%$busqueda%' 
+                     OR usuario_apellido LIKE '%$busqueda%' 
+                     OR usuario_email LIKE '%$busqueda%' 
+                     OR usuario_usuario LIKE '%$busqueda%'))";
+
+                }else{
+
+                    $consulta_datos="SELECT * FROM usuario WHERE usuario_id!='".$_SESSION['id']."' AND usuario_id!='1' ORDER BY usuario_nombre ASC LIMIT $inicio,$registros";
+
+                    $consulta_total="SELECT COUNT(usuario_id) FROM usuario WHERE usuario_id!='".$_SESSION['id']."' AND usuario_id!='1'";
+                }
+
+                $datos = $this->ejecutarConsulta($consulta_datos);
+                $datos = $datos->fetchAll();
+
+                $total = $this->ejecutarConsulta($consulta_total);
+                $total = (int) $total->fetchColumn();
+
+                $numeroPaginas=ceil($total/$registros);
+
+                $tabla.='
+                <div class="table-container">
+                <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+                    <thead>
+                        <tr>
+                            <th class="has-text-centered">#</th>
+                                <th class="has-text-centered">Nombre</th>
+                                    <th class="has-text-centered">Usuario</th>
+                                        <th class="has-text-centered">Email</th>
+                                    <th class="has-text-centered">Creado</th>
+                                <th class="has-text-centered">Actualizado</th>
+                            <th class="has-text-centered" colspan="3">Opciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    ';
+
+                    if($total>=1 && $pagina<=$numeroPaginas){
+                        $contador=$inicio+1;
+                        $pag_inicio=$inicio+1;
+                        foreach($datos as $rows){
+                            $tabla.='
+                            <tr class="has-text-centered">
+					        <td>'.$contador.'</td>
+					        <td>'.$rows['usuario_nombre'].'
+                                '.$rows['usuario_apellido'].'</td>
+					        <td>'.$rows['usuario_usuario'].'</td>
+					        <td>'.$rows['usuario_email'].'</td>
+					        <td>'.date("d-m-Y h:i:s A"
+                            ,strtotime($rows['usuario_creado'])).'</td>
+					        <td>'.date("d-m-Y h:i:s A"
+                            ,strtotime($rows['usuario_actualizado'])).'</td>
+					        <td>
+	                            <a href="'.APP_URL.'userPhoto/'.$rows['usuario_id'].'/
+                                " class="button is-info is-rounded is-small">Foto</a>
+	                        </td>
+	                        <td>
+	                            <a href="'.APP_URL.'userUpdate/'.$rows['usuario_id'].'/" class="button is-success is-rounded is-small">Actualizar</a>
+	                        </td>
+	                        <td>
+	                	        <form class="FormularioAjax" action="
+                                '.APP_URL.'app/ajax/usuarioAjax.php" method="POST" autocomplete="off">
+
+	                		        <input type="hidden" name="modulo_usuario" value="eliminar">
+	                		        <input type="hidden" name="usuario_id" value="'.$rows['usuario_id'].'">
+
+	                    	        <button type="submit" class="button is-danger is-rounded is-small">Eliminar</button>
+	                            </form>
+	                        </td>
+				        </tr>
+                            ';
+                            $contador++;
+                        }
+                        $pag_final=$contador-1;
+                    }else{
+                        if($total>=1){
+                            $tabla.='
+                            <tr class="has-text-centered" >
+	                            <td colspan="7">
+	                                <a href="'.$url.'1/" class="button is-link is-rounded is-small mt-4 mb-4">
+	                                    Haga clic acá para recargar el listado
+	                                </a>
+	                            </td>
+	                        </tr>
+                            ';
+                        }else{
+                            $tabla.='
+                            <tr class="has-text-centered" >
+	                            <td colspan="7">
+	                                No hay registros en el sistema
+	                            </td>
+	                        </tr>
+                            ';
+                        }
+                    }
+
+                    $tabla.='</tbody></table></div>';
+
+                    if($total>=1 && $pagina<=$numeroPaginas){
+                        $tabla='.<p class="has-text-right">
+                        Mostrando usuarios <strong>'.$pag_inicio.'</strong> 
+                        al <strong>'.$pag_final.'</strong> 
+                        de un <strong>total 
+                        de '.$total.'</strong></p>.';
+
+                        $tabla.=$this->paginadorTablas($pagina,$numeroPaginas,$url,10);
+                    }
+                    return $tabla;
+        }
     }
---
+++ app/views/content/userList-view.php
@@ -0,0 +1,34 @@
+<div class="container is-fluid mb-6">
+	<h1 class="title">Usuarios</h1>
+	<h2 class="subtitle">Lista de usuario</h2>
+</div>
+<div class="container pb-6 pt-6">
+
+
+	
+    
+				
+
+				
+
+	            
+			   
+
+
+
+
+
+
+	
+
+	<nav class="pagination is-centered is-rounded" role="navigation" aria-label="pagination">
+    	<a class="pagination-previous is-disabled" disabled="">Anterior</a>
+
+	    <ul class="pagination-list">
+	    	<li><a class="pagination-link is-current" href="#">1</a></li>
+	    </ul>
+
+    	<a class="pagination-next is-disabled" disabled="">Siguiente</a>
+    </nav>
+
+</div>
